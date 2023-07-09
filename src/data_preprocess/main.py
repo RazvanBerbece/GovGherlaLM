e03271e76490c01c908f4fc0e94cf3e10c66ebeb
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2023-07-09 14:14:11 +0100

    Add facebook folder in data scope

--- src/data_preprocess/main.py
+++ src/data_preprocess/main.py
@@ -1,12 +1,12 @@
 from parser.parser import Parse
 
 # Data Preprocessing Parameters
-raw_data_source_folder = "../../data/raw/facebook-100002107045945/messages/inbox/guvernulgherla_7283751518362871/"
-output_folder = "../../data/clean/"
+raw_data_source_folder = "../../data/facebook/raw/facebook-100002107045945/messages/inbox/guvernulgherla_7283751518362871/"
+output_folder = "../../data/facebook/clean/"
 
 # Extract messages from raw data
 messages = Parse() \
-    .from_source_folder("../../data/raw/facebook-100002107045945/messages/inbox/guvernulgherla_7283751518362871/") \
+    .from_source_folder(raw_data_source_folder) \
     .and_debugging_enabled() \
     .execute()
 
